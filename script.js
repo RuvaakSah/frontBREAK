d5860ccaaaa56fecc9e1da7d8ec691959702eab7
Juan M <juanmegiasmarkelov@gmail.com>
2026-04-08 15:03:56 +0200

    TRABAJO

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const API_URL = "https://backbreak.onrender.com"; 
 
+
 async function getTasks() {
     const res = await fetch(API_URL);
     const tasks = await res.json();
     const list = document.getElementById('taskList');
-    list.innerHTML = tasks.map(t => `<li>${t.title} <button onclick="deleteTask('${t._id}')">X</button></li>`).join('');
+    list.innerHTML = '';
+    tasks.forEach(task => {
+        list.innerHTML += `
+            <li>
+                ${task.title}
+                <button class="del-btn" onclick="deleteTask('${task._id}')">Eliminar</button>
+            </li>
+        `;
+    });
 }
 
 async function addTask() {
     const input = document.getElementById('taskInput');
+    if (!input.value.trim()) return;
+
     await fetch(API_URL, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ title: input.value })
     });
     input.value = '';
